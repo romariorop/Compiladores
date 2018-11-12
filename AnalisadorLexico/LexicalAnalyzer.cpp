9fbbf2ffcfafb2af0076e07af285f465666abd2a
romario <romariopantaleao@gmail.com>
2018-11-12 08:05:04 -0300

    fixing counter

--- AnalisadorLexico/LexicalAnalyzer.cpp
+++ AnalisadorLexico/LexicalAnalyzer.cpp
@@ -1,5 +1,6 @@
 #include <map>
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <deque>
 #include <regex>
@@ -93,7 +94,7 @@ bool LexicalAnalyzer::hasToken(){
 		index = 0;
 		if(getline(infile, nextLine)){
 			while(nextLine[pos] == ' ' || nextLine[pos] == '\t')++pos;
-			if(pos < (int)nextLine.size())std::cout << line+1  << "- ";
+			if(pos < (int)nextLine.size())printf("%04d  ", line+1);
 			while(pos < (int)nextLine.size()){
 				std::cout << nextLine[pos];
 				++pos;
